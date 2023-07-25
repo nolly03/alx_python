16a7f852365fb19fc2d3ec7aa4d4238cf1d2113d
Nolly <nolly9662@gmail.com>
2023-07-25 10:47:03 +0100

    task 3, last digit number

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, "is positive\n", end="\n")
+    print(number, "is positive", end="\n")
 elif number == 0:
     print(number, "is zero", end="\n")
 elif number < 0:
