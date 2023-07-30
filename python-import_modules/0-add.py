0e8f2f212f0e9b7572aaa166823fc6c6bc3474dd
Nolly <nolly9662@gmail.com>
2023-07-30 13:33:29 +0100

    usf

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,10 @@
 # Importing the add function from add_0.py
 from add_0 import add
 
+# This is the add function that performs addition
+def add(a, b):
+    return a + b
+
 # Assigning values to variables a and b
 a = 1
 b = 2
