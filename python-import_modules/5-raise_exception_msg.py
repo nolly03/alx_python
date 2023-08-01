d19d9f064af171f9887abb4ade2b5dadbac5432f
Nolly <nolly9662@gmail.com>
2023-08-01 23:03:14 +0100

    gdghdghd

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,8 @@
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
+# Test the function
+try:
+    raise_exception_msg("This is a custom name exception")
+except NameError as e:
+    print("Exception caught:", e)
