ce874c4885089fb88f87ff85231755ffe3ac2750
Nolly <nolly9662@gmail.com>
2023-08-09 19:38:20 +0100

    hdhdh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,13 +2,13 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
+    #getting the area
     def area(self):
         return self.__size ** 2
-
+    # getting the perimeter
     def perimeter(self):
         return 4 * self.__size
-
+    # getting the size
     def get_size(self):
         return self.__size
 
