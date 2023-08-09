95afa1ab3e39f330b8015478b3200b26015a17d3
Nolly <nolly9662@gmail.com>
2023-08-09 19:34:30 +0100

    verified

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,17 @@
-class square:
-    __size = size
-def __init__(self, size):
-    self.size = size
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def perimeter(self):
+        return 4 * self.__size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
+
