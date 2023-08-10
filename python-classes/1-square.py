def1e7f542d61814c2b9fd6c22b2857583e7d9f6
Nolly <nolly9662@gmail.com>
2023-08-10 11:05:46 +0100

    eee

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,13 @@
 class Square:
-    def __init__(self, size=0):
+    def __init__(self, size=0): #initializing the class
+        # raising exception
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         
         self.__size = size
-
+       #defining areag
     def area(self):
         return self.__size ** 2
 
