bf0d3423213fe07ab065fc0270b80b15c6cbe032
offordile chinonso <nolly9662@gmail.com>
2023-09-08 14:06:08 +0100

    corrected

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# defines a rectangle class
+"""defines a rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    # representing rectangle
+    """representing rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# defines a square class
+"""defines a square class"""
 from models.rectangle import Rectangle
 
 
